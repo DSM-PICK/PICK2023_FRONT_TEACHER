2430ad8e71c1a04ec8ed9fbac8f6052b26d241aa
Yeong joon <bluehome8626@naver.com>
2023-01-21 20:43:09 +0900

    📑 :: tab에 쓰일 image 추가

---
+++ src/assets/tab/index.ts
@@ -0,0 +1,6 @@
+export { default as offAttendance } from "./offAttendance.svg";
+export { default as offCheck } from "./offCheck.svg";
+export { default as offList } from "./offList.svg";
+export { default as onAttendance } from "./onAttendance.svg";
+export { default as onCheck } from "./onCheck.svg";
+export { default as onList } from "./onList.svg";
