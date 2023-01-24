83f9632e728b2d8b7aea2271208d5a47fceaa354
Kangyongsoo <aodtn2006@naver.com>
2023-01-24 22:26:31 +0900

    ⚡️ :: (#3) PICK 선생님 출석확인 상세 페이지 퍼블리싱

---
+++ src/components/studentState/index.tsx
@@ -0,0 +1,72 @@
+import { Theme } from "@emotion/react";
+import styled from "@emotion/styled";
+import { useEffect, useRef } from "react";
+
+interface StateProps {
+  number: string;
+  name: string;
+  state: string;
+  isMove?: string;
+}
+
+const studentState = ({ number, name, state, isMove }: StateProps) => {
+  const Ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const { current } = Ref;
+    if (current !== null) {
+      switch (state) {
+        case "이동":
+        case "외출": {
+          current.style.backgroundColor = "#9650fa";
+          current.style.color = "#ffffff";
+          current.style.border = "0";
+          break;
+        }
+        case "현체":
+        case "취업": {
+          current.style.backgroundColor = "#F0e6ff";
+          break;
+        }
+        case "무단": {
+          current.style.backgroundColor = "#f04d51";
+          current.style.border = "0";
+          current.style.color = "#ffffff";
+          break;
+        }
+
+        default:
+          break;
+      }
+    }
+  });
+  return (
+    <Container>
+      <div>
+        {number} {name}
+      </div>
+      <div>{isMove ? isMove : "-"}</div>
+      <StateBox ref={Ref}>{state}</StateBox>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const StateBox = styled.div`
+  width: 100px;
+  height: 32px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f9f7fa;
+  border: 1px solid #b7b4bd;
+  border-radius: 8px;
+  color: #5c5961;
+`;
+
+export default studentState;
