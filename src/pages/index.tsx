cad0f31bc6dbfec95a46bb192aeb05cd180d13f9
김한비 <gksql2256@dsm.hs.kr>
2023-01-24 23:29:16 +0900

    ⚡️:: (#5) PICK선생님 출석확인 페이지 퍼블리싱 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import Start from "@/components/start";
-import Check from "@/components/check";
+
 
 export default function Home() {
   return (
     <>
     <Start/>
-      <Check />
     </>
   );
 }
