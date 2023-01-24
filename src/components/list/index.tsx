adc9d1452eca368d50e11b4bca2c329f0d38ecc2
z <justinlee05@naver.com>
2023-01-24 19:33:39 +0900

    ⚡️ :: (#9) PICK 외출자 목록 페이지 퍼블리싱

---
+++ src/components/list/index.tsx
@@ -0,0 +1,51 @@
+import styled from "@emotion/styled";
+import StudentBlock from "./student";
+
+const OutList = () => {
+  return (
+    <Wrapper>
+      <TitleContainer>
+        <TitleText>외출자 목록</TitleText>
+      </TitleContainer>
+      <StudentContainer>
+        <StudentBlock gcn={1234} returnTime="19:30" studentName="이경수" />
+        <StudentBlock gcn={1234} returnTime="19:30" studentName="이경수" />
+        <StudentBlock gcn={1234} returnTime="19:30" studentName="이경수" />
+        <StudentBlock gcn={1234} returnTime="19:30" studentName="이경수" />
+        <StudentBlock gcn={1234} returnTime="19:30" studentName="이경수" />
+        <StudentBlock gcn={1234} returnTime="19:30" studentName="이경수" />
+        <StudentBlock gcn={1234} returnTime="19:30" studentName="이경수" />
+        <StudentBlock gcn={1234} returnTime="19:30" studentName="이경수" />
+        <StudentBlock gcn={1234} returnTime="19:30" studentName="이경수" />
+      </StudentContainer>
+    </Wrapper>
+  );
+};
+
+export default OutList;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0 16px;
+`;
+
+const TitleContainer = styled.div`
+  display: flex;
+  padding: 14px 0;
+`;
+
+const TitleText = styled.h1`
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 28px;
+`;
+
+const StudentContainer = styled.div`
+  height: fit-content;
+  padding: 16px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
+`;
