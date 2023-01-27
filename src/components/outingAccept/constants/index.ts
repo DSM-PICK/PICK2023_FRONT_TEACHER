d256d95bedbfbf3e4a909c4cc23efc48e6918f6e
안윤지 <dksdbswl10430@naver.com>
2023-01-27 21:23:35 +0900

    ⚡️:: (#14) 외출신청리스트 constants로 분리

--- src/components/outingAccept/constants/index.ts
+++ src/components/outingAccept/constants/index.ts
@@ -1,2 +1,45 @@
-export const gradeNumArr = ["1학년", "2학년", "3학년"];
-export const classNumArr = ["1반", "2반", "3반", "4반"];
+export const gradeNumArr = [
+  { title: "1학년", name: "gradeNum" },
+  { title: "2학년", name: "gradeNum" },
+  { title: "3학년", name: "gradeNum" },
+];
+
+export const classNumArr = [
+  { title: "1반", name: "classNum" },
+  { title: "2반", name: "classNum" },
+  { title: "3반", name: "classNum" },
+  { title: "4반", name: "classNum" },
+];
+
+export const outingRequestList = [
+  {
+    name: "2101 강석현",
+    time: "16:30 ~ 18:30",
+    reason: "저 꼬리뼈 다쳐서 정형외과 다녀와야할 것 같아요 보내주세요 찡찡",
+    id: 1,
+  },
+  {
+    name: "2102 김경호",
+    time: "16:30 ~ 19:30",
+    reason: "저 안경이 부러졌어요",
+    id: 2,
+  },
+  {
+    name: "2106 김의찬",
+    time: "16:30 ~ 20:30",
+    reason: "이유가 없습니다.",
+    id: 3,
+  },
+  {
+    name: "2110 문정민",
+    time: "16:30 ~ 19:30",
+    reason: "이유가 없습니다.",
+    id: 4,
+  },
+  {
+    name: "2120 추혜연",
+    time: "16:30 ~ 20:00",
+    reason: "이유가 없습니다.",
+    id: 5,
+  },
+];
