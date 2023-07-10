80f04c0e2877dbb83b4ce270f80cd22b24c49c84
inung1004 <dksdbswl10430@naver.com>
2023-07-10 23:00:53 +0900

    ⚡️ :: 담당반 조회 API 세팅

--- src/utils/api/common/index.ts
+++ src/utils/api/common/index.ts
@@ -1,3 +1,4 @@
+import { GetMyClassResponseDto } from "@/models/common";
 import instance from "@/utils/axios";
 
 interface GetDateTypeDto {
@@ -6,6 +7,15 @@ interface GetDateTypeDto {
 }
 
 export const getDateType = async (date: string) => {
-  const dateType = await instance.get<GetDateTypeDto>(`/pick/admin/?date=${date}`);
+  const dateType = await instance.get<GetDateTypeDto>(
+    `/pick/admin/?date=${date}`
+  );
   return dateType;
+};
+
+export const getMyClass = async () => {
+  const response = await instance.get<GetMyClassResponseDto>(
+    `/pick/teachers/buck`
+  );
+  return response.data;
 };
